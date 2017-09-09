59c7dc3f8f3265f63b64ccc9d23d29ea7080b366
jhalep <jhalep2@gmail.com>
2017-09-09 16:39:42 -0500

    Add audioResourceId attribute to word class and set audios for NumberstActivity

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -30,6 +30,9 @@ public class Word {
     /** Image resource ID for the word */
     private int mImageResourceId = NO_IMAGE_PROVIDED;
 
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
     /** Constant value that represents no image was provided for this word */
     private static final int NO_IMAGE_PROVIDED = -1;
 
@@ -54,6 +57,13 @@ public class Word {
      * @param imageResourceId is the drawable resource ID for the image associated with the word
      *
      */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId,int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
     public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
@@ -72,6 +82,13 @@ public class Word {
      */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getAudioResourceId() {
+        return mAudioResourceId;
     }
 
     /**
